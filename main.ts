d96caa1fea5011d5ffb6d27c75347301703892d8
deepandahal <69980408+deepandahal@users.noreply.github.com>
2020-08-26 16:54:50 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.No)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Sad)
+})
